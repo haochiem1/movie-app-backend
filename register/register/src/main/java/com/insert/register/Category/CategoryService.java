f4326f062d93665886044f1de9f73fe63f192d7b
Minh Nguyen <minhg89@gmail.com>
2022-11-17 13:43:55 -0500

    category added

---
+++ register/register/src/main/java/com/insert/register/Category/CategoryService.java
@@ -0,0 +1,12 @@
+package com.insert.register.Category;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import javax.mail.MessagingException;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface CategoryService {
+    
+}
---
+++ register/register/src/main/java/com/insert/register/Category/CategoryServiceImpl.java
@@ -0,0 +1,24 @@
+package com.insert.register.Category;
+
+import com.insert.register.Security.*;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CategoryServiceImpl {
+    @Autowired
+    private CategoryRepository movieRepository;
+}
+
